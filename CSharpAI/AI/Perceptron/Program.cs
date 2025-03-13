019357c511a71323da82b060b3cb6d9995978f48
SagiKPJH1021 <juhyung1021@gmail.com>
2025-03-13 20:59:10 +0900

    feat : add perceptron gate

--- CSharpAI/AI/Perceptron/Program.cs
+++ CSharpAI/AI/Perceptron/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+using Perceptron.Gate;
+
+Console.WriteLine("Perceptron Gate vs Binary Gate");
+var summary = BenchmarkRunner.Run<Benchmark>();
