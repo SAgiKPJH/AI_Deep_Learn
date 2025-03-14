fc39c99be17152db47bb58fed5cea91361c0a43f
SagiKPJH1021 <juhyung1021@gmail.com>
2025-03-14 16:57:55 +0900

    faet : add semantic-kernal

--- Semantic-Kernal/Example/SemanticKernal_Example/Program.cs
+++ Semantic-Kernal/Example/SemanticKernal_Example/Program.cs
@@ -1,6 +1,15 @@
-﻿
-
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
 
 var modelId = "";
 var endpoint = "";
 var apiKey = "";
+
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+var kernel = builder.Build();
+
+IChatCompletionService chatCompletion = kernel.GetRequiredService<IChatCompletionService>();
+kernel.Plugins.AddFromType<>("Lights");
--- Semantic-Kernal/Example/SemanticKernal_Example/ReadMe.md
+++ Semantic-Kernal/Example/SemanticKernal_Example/ReadMe.md
@@ -1,5 +1,6 @@
 # Semantic Kernel
 - https://github.com/microsoft/semantic-kernel
+- https://learn.microsoft.com/en-us/semantic-kernel/get-started/quick-start-guide?pivots=programming-language-csharp
 
 # Nuget Package
 - Microsoft.SemanticKernel
